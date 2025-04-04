25aa582593e08023246360537bccc21564fbfa93
Danilopelaso-ERNI <danilo.pelaso@betterask.erni>
2025-04-04 13:50:04 +0800

    asd

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yey, see you on Feb 14!";
+  question.innerHTML = "";
   gif.src =
     "https://media.giphy.com/media/w6Oh9RI9lSD9S/giphy.gif?cid=790b7611xm2wjrb1cc72ry1ozwp898llybbgtwi5ymwepu3c&ep=v1_gifs_search&rid=giphy.gif&ct=g";
 });
